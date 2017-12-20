5346e749f76a9b61a5bccee850c9049534e8094c
Aaron Johnson <juice.johnson@gmail.com>
2017-12-19 23:58:04 -0600

    revert back to THU-MON

--- resources/views/lineups-fd-nfl.blade.php
+++ resources/views/lineups-fd-nfl.blade.php
@@ -42,7 +42,7 @@
         <p class="initial-hide">
           You are viewing the <b>@{{current_slate_text}} slate</b>.
            Change to <a style="text-decoration: underline;" href="@{{change_slate_link}}">@{{change_slate_text}} slate</a>.
-          <p>Also consider: <a style="text-decoration: underline;" href="/lineup-generator-nfl">DraftKings NFL SAT ONLY slate</a> or <a style="text-decoration: underline;" href="/lineup-generator-nfl/main">DraftKings NFL Main slate</a></p>
+          <p>Also consider: <a style="text-decoration: underline;" href="/lineup-generator-nfl">DraftKings NFL THU-MON slate</a> or <a style="text-decoration: underline;" href="/lineup-generator-nfl/main">DraftKings NFL Main slate</a></p>
         </p>
       </div>
       <header>
