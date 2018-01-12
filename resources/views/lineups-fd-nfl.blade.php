650ab39b4fe99be7d0acddf8cd3ebc2ce8dc1fe5
Aaron Johnson <ajj@umn.edu>
2018-01-12 10:42:09 -0600

    w19 updates for DK

--- resources/views/lineups-fd-nfl.blade.php
+++ resources/views/lineups-fd-nfl.blade.php
@@ -43,7 +43,7 @@
           You are viewing the <b>@{{current_slate_text}} slate</b>.
            Change to <a style="text-decoration: underline;" href="@{{change_slate_link}}">@{{change_slate_text}} slate</a>.
           <p>Also consider:
-            <!-- <a style="text-decoration: underline;" href="/lineup-generator-nfl">DraftKings NFL THU-MON slate</a> or  -->
+            <!-- <a style="text-decoration: underline;" href="/lineup-generator-nfl">DraftKings NFL SUN ONLY slate</a> or  -->
             <a style="text-decoration: underline;" href="/lineup-generator-nfl/main">DraftKings NFL Main slate</a></p>
         </p>
       </div>
