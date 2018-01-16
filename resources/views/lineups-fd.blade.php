249bda351d9c20a68a0f707918f8f4b6c1c7be63
Aaron Johnson <ajj@umn.edu>
2018-01-16 11:53:34 -0600

    update FD view

--- resources/views/lineups-fd.blade.php
+++ resources/views/lineups-fd.blade.php
@@ -115,7 +115,7 @@
           <table class="players">
             <thead>
               <tr>
-                <th>Player</th>
+                <th style="width: 55%;">Player</th>
                 <th>Salary</th>
                 <th>Target %</th>
                 <th>Target Ct</th>
@@ -125,7 +125,7 @@
             </thead>
             <tbody>
               <tr v-for="player in players">
-                <td class="fgi-player-name"><span class="initial-hide">@{{player.name}} <span class="pull-right">@{{player.tee_1}}<span v-if="player.tee_2.length > 0"> / @{{player.tee_2}}</span></span></span></td>
+                <td class="fgi-player-name" style="width: 55%;"><span class="initial-hide">@{{player.name}} <span class="pull-right" style="font-size: 90%;">@{{player.tee_1}}<span v-if="player.tee_2.length > 0"> / @{{player.tee_2}}</span><span v-if="player.tee_3.length > 0"> / @{{player.tee_3}}</span></span></span></td>
                 <td class="fgi-player-salary"><span class="initial-hide">$@{{player.salary}}</span></td>
                 <td class="fgi-player-rank"><span class="initial-hide"><input class="exposure-item" v-on:change='update()' style="font-size: 12px; text-align:center; width:55px; height: 20px; border:1px solid #979797;" min="0" v-model="player.weight" type="number"></span></td>
                 <td><span class="initial-hide">@{{player.totalSpots = ((player.weight/100) * lineups).toFixed(1)}}</span></td>
