91d518bbf8683f98dbdcf39f8f526032ff42fbd3
Aaron Johnson <juice.johnson@gmail.com>
2018-02-06 23:13:33 -0600

    pebble beach tee times and course rotations

--- resources/views/lineups.blade.php
+++ resources/views/lineups.blade.php
@@ -112,20 +112,23 @@
       <div class="tab-content">
         <div role="tabpanel" class="tab-pane active" id="players-wrapper">
           <div class="row" style="margin-top: -8px; margin-bottom: 2px;">
-            <div class="col-md-8 col-sm-8 col-xs-8" v-if="players[0].tee_3.length > 0">
-              <b>Course:</b>
+            <div class="col-md-8 col-sm-8 col-xs-8" v-if="players[0].tee_4.length > 0">
+              <b>Course Rotations:</b> (First day, second day, third day)
             </div>
             <div class="pull-right col-md-4 col-sm-4 col-xs-4">
               <small class="pull-right"><a href="#" id="clear-exposure" v-on:click="clear()">[X] Clear Exposure</a></small>
             </div>
           </div>
-          <div class="row" style="margin-top: -8px; margin-bottom: 2px;" v-if="players[0].tee_3.length > 0">
+          <div class="row" style="margin-top: -8px; margin-bottom: 2px;" v-if="players[0].tee_4.length > 0">
             <div class="col-md-12 col-sm-12 col-xs-12">
-              <small>
-                <span class="label label-success">SC</span> = Torrey Pines GC (South)
+              <small style="display: block; margin-bottom: 5px;">
+                <span class="label label-success">PB</span> = Pebble Beach, Spyglass Hill, Monterey Peninsula
               </small>
-              <small style="margin-left: 10px;">
-                <span class="label label-info">NC</span> = Torrey Pines GC (North)
+              <small style="display: block; margin-bottom: 5px;">
+                <span class="label label-info">SH</span> = Spyglass Hill, Monterey Peninsula, Pebble Beach
+              </small>
+              <small style="display: block; margin-bottom: 5px;">
+                <span class="label label-warning">MP</span> = Monterey Peninsula, Pebble Beach, Spyglass Hill
               </small>
             </div>
           </div>
@@ -142,7 +145,7 @@
             </thead>
             <tbody>
               <tr v-for="player in players">
-                <td class="fgi-player-name" style="width: 55%;"><span v-if="player.tee_3.length > 0" v-html="player.tee_3"></span><span class="initial-hide">@{{player.name}} <span class="pull-right" style="font-size: 95%;">@{{player.tee_1}}<span v-if="player.tee_2.length > 0"> / @{{player.tee_2}}</span></span></span></td>
+                <td class="fgi-player-name" style="width: 55%;"><span v-if="player.tee_4.length > 0" v-html="player.tee_4"></span><span class="initial-hide">@{{player.name}} <span class="pull-right" style="font-size: 95%;">@{{player.tee_1}}<span v-if="player.tee_2.length > 0"> / @{{player.tee_2}}</span></span></span></td>
                 <td class="fgi-player-salary"><span class="initial-hide">$@{{player.salary}}</span></td>
                 <td class="fgi-player-rank"><span class="initial-hide"><input class="exposure-item" v-on:change='update()' style="font-size: 12px; text-align:center; width:55px; height: 20px; border:1px solid #979797;" min="0" v-model="player.weight" type="number"></span></td>
                 <td><span class="initial-hide">@{{player.totalSpots = ((player.weight/100) * lineups).toFixed(1)}}</span></td>
